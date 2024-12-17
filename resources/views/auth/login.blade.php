a868677e2945a329b40909d8980d843b3e43324f
Matheus Afornali <afornalimatheus@gmail.com>
2024-12-17 05:58:20 -0300

    create login method for validate user data input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,12 @@
 <div>
     <h1>Login</h1>
 
-    <div>
-        
-    </div>
+    <form action="/login" method="POST">
+        @csrf
+
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
+        <input type="password" name="password" placeholder="Senha">
+
+        <button>Logar</button>
+    </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', WelcomeController::class);
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())->middleware('auth')->name('dashboard');
