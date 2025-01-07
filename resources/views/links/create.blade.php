c1960f48556d442e0f9c6b2d7f699bebca50b264
Matheus Afornali <afornalimatheus@gmail.com>
2025-01-07 05:17:01 -0300

    create logout route and model link to create new links

---
+++ resources/views/links/create.blade.php
@@ -0,0 +1,27 @@
+<div>
+    {{ auth()->id() }}
+    <h1>Criar um link</h1>
+
+    @if ($message = session()->get('message'))
+        <div>
+            <div>{{ $message }}</div>
+        </div>
+    @endif
+
+    <form action="{{ route('links.create') }}" method="POST">
+        @csrf
+        <div>
+            <input type="link" name="link" placeholder="Link"  value="{{ old('link') }}" />
+            @error('link')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <div>
+            <input type="name" name="name" placeholder="Nome" value="{{ old('name') }}" />
+            @error('name')
+                <span>{{ $message }}</span>   
+            @enderror
+        </div>
+        <button>Salvar</button>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LinkController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class);
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('guest')->group(function () {
+  Route::get('/login', [LoginController::class, 'index'])->name('login');
+  Route::post('/login', [LoginController::class, 'login']);
+  Route::get('/register', [RegisterController::class, 'index'])->name('register');
+  Route::post('/register', [RegisterController::class, 'register']);
+});
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
+Route::middleware('auth')->group(function () {
+  Route::get('/logout', LogoutController::class)->name('logout');
+  Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())->name('dashboard');
 
-Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())->middleware('auth')->name('dashboard');
+  Route::get('/links/create', [LinkController::class, 'create'])->name('links.create');
+  Route::post('/links/create', [LinkController::class, 'store']);
+});
